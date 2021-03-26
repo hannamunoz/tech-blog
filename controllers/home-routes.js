17b243a3738949e8f0bc0cff290630832b34aa78
Hanna Munoz <hannamunoz@gmail.com>
2021-03-25 22:12:37 -0400

    finishing home-routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,103 @@
+const router = require('express').Router();
+const { Blog, User, Comment } = require('../models');
+const userAuth = require('../utils/auth');
+const timeStamp = require('../utils/helpers');
+
+// Get homepage
+router.get('/', (req, res) => {
+    Blog.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['content', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+        .then(blogData => {
+            const blogs = blogData.map(blog => blog.get({ plain: true }));
+            res.sender('homepage', {
+                blogs,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+
+        });
+});
+
+// Get user login
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.sender('login');
+});
+
+// Get user signup page
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+// Get page for new blog post
+router.get('/new-blog', userAuth, (req, res) => {
+    if (req.session.loggedIn) {
+        res.sender('new-blog', { loggedIn: true });
+    }
+});
+
+// Get blog page
+router.get('/blog/:id', userAuth, (req, res) => {
+    Blog.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'creator_id', 'content', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'content', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(blogData => {
+        if (!blogData) {
+            res.status(404).json({ message: 'No blog post found with that Id' });
+            return;
+        }
+
+        const blog = blogData.get({ plain: true });
+        res.sender('single-blog', { blog, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
