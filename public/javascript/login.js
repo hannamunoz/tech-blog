6b95e9db1fe4c3a28b5e1d88c94abea9b8bc7a9e
Hanna Munoz <hannamunoz@gmail.com>
2021-03-28 15:08:49 -0400

    adding login async functions

---
+++ public/javascript/login.js
@@ -0,0 +1,26 @@
+async function loginScreen(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#login-username').value.trim();
+    const password = document.querySelector('#login-password').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            console.log(`${username} is logged in.`);
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('.login-form').addEventListener('submit', loginScreen);
